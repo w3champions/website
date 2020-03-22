47c0b2eb0016c4dde30f696308cd6ad4e51cc31a
Kato88 <yamiyasha@freenet.de>
2020-03-22 12:03:12 +0100

    restructured home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,176 +2,98 @@
   <v-container fluid>
     <v-row>
       <v-col cols="12" md="8">
-
-
-
-<v-card tile>
-  <v-card-title></v-card-title>
-<v-row>
-  <v-col cols="8">
-    
-  </v-col>
-  <v-col cols="4">
-  </v-col>
-</v-row>
-</v-card>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <v-row>
-          <v-col cols="12">
-            <v-card tile>
-<v-row>
-<v-col cols="6">
-              <v-card-text>
-                <h3>It is finally there - your Ladder for Warcraft III</h3>
-                <div>
-                  Finally your Warcraft III 1on1 Ladder is available, crafted by and for the community!
-                  <br><br>
-                  The ladder contains:
-  <ul>
-    <li>Improved Pings by non-global ladder</li>
-    <li>Matchmaking (based on W3Arena)</li>
-    <li>Ladder System (Check the RANKINGS tab top right)</li>
-    <li>Seamless integration in your Warcraft III client</li>
-    <li>Profiles</li>
-    <li>Different Mappool</li>
-  </ul>
-
-
-<div>
-  <br/>
-                  <b>Current Map Pool</b><br>
-                    TM,EI,TSLV,CH,NIS,AZ and LR
-                    </div>
-                </div>
-              </v-card-text>
-              </v-col>
-              <v-col cols="6" style="text-align: center">
-                <img src="../assets/reforged.jpg" style="max-width: 90%; max-height: 320px;">
-              </v-col>
-</v-row>
-
-              <v-row>
-                <v-col cols="6">
-                  <v-card-text>
-                    <h3>Installation Guide Windows</h3>
-                    <p>We created a setup tool that automatically does all steps needed.</p>
-                    <v-btn
-                      color="primary"
-                      href="./w3champions-setup.exe"
-                      target="_blank"
-                      value
-                      outlined
-                      class="w3-background"
-                    >
-                      <v-icon>mdi-download</v-icon>
-                      <span class="mr-2 hidden-xs-only">Download the Windows installer</span>
-                    </v-btn>
-                                        <br/><br/>
-                    <p>
-                      <ol>
-                        <li>
-                      Download the installer, execute it and select your Warcraft III installation folder.
-                     </li>
-                      <li>
-                       You are ready to go. Start Warcraft III and you should see the new button in the Versus-screen.
-                     </li>
-                    </ol>
-                    </p>
-                    <p>
-                    <b>Issues with the download link?</b><br/> Some Antivir-Software has falsely detected our setup as malicious. We contacted them and some already corrected their databases. If thats not the case for you, use the following link and unpack the setup with the <b style="white-space: nowrap;">password WC3</b>
-                    </p>
-                                        <v-btn
-                                        class="w3-background"
-                      color="secondary"
-                      href="./w3champions-setup.zip"
-                      target="_blank"
-                      value
-                      outlined
-                    >
-                      <v-icon>mdi-download</v-icon>
-                      <span class="mr-2 hidden-xs-only">Download the Windows installer zipped</span>
-                    </v-btn>
-                  </v-card-text>
-                </v-col>
-                <v-col cols="6">
-                  <v-card-text>
-                    <h3>Installation Guide Mac OS</h3>
-                    <p>With a Mac you have to place the following files in your WC3 Installation folder manually.</p>
-                    <v-btn
-                    class="w3-background"
-                      color="primary"
-                      href="./webui.zip"
-                      target="_blank"
-                      outlined
-                    >
-                      <v-icon>mdi-download</v-icon>
-                      <span class="mr-2 hidden-xs-only">Download the Mac archive</span>
-                    </v-btn>
-                    <br/><br/>
-                    <p>
-                      <ol>
-                        <li>
-                      Download the archive file and unpack it in your Warcraft Installation folder
-                     </li>
-                     <li>
-                      Make sure to have a directory structure like Warcraft III\webui\index.html
-                     </li>
-                      <li>
-                       You are ready to go. Start Warcraft III and you should see the new button in the Versus-screen.
-                     </li>
-                    </ol>
-                    </p>
-                  </v-card-text>
-                </v-col>
-              </v-row>
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" style="display:none">
-            <v-card>
-              <v-card-title>Lastest update</v-card-title>
-              <v-card-text>
-                <p>New update released. The following features are now available:</p>
+        <v-card tile>
+          <v-card-title>It is finally there - your Ladder for Warcraft III</v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12" md="6">
+                Finally your Warcraft III 1on1 Ladder is available, crafted by and for the community!
+                <br />
+                <br />The ladder contains:
                 <ul>
-                  <li>Find opponents in 1on1 (no MMR yet, just randomly putting together players)</li>
-                  <li>Select race</li>
-                  <li>record of wins and losses</li>
-                  <li>profile page: http://profile.w3champions.com/</li>
+                  <li>Improved Pings by non-global ladder</li>
+                  <li>Matchmaking (based on W3Arena)</li>
+                  <li>Ladder System (Check the RANKINGS tab top right)</li>
+                  <li>Seamless integration in your Warcraft III client</li>
+                  <li>Profiles</li>
+                  <li>Different Mappool</li>
                 </ul>
-              </v-card-text>
-              <v-card-text>
+                <br />
+                <b>Current Map Pool</b>
+                <br />TM,EI,TSLV,CH,NIS,AZ and LR
+              </v-col>
+              <v-col cols="12" md="6">
+                <img src="../assets/reforged.jpg" style="max-width: 90%; max-height: 320px;" />
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+          <v-card-text>
+            <v-row>
+              <v-col cols="6" md="6">
+                <h3>Installation Guide Windows</h3>
+                <p>We created a setup tool that automatically does all steps needed.</p>
+                <ol>
+                  <li>Download the installer, execute it and select your Warcraft III installation folder.</li>
+                  <li>You are ready to go. Start Warcraft III and you should see the new button in the Versus-screen.</li>
+                </ol>
+              </v-col>
+              <v-col cols="6" md="6">
+                <h3>Installation Guide Mac OS</h3>
+                <p>With a Mac you have to place the following files in your WC3 Installation folder manually.</p>
+                <ol>
+                  <li>Download the archive file and unpack it in your Warcraft Installation folder</li>
+                  <li>Make sure to have a directory structure like Warcraft III\webui\index.html</li>
+                  <li>You are ready to go. Start Warcraft III and you should see the new button in the Versus-screen.</li>
+                </ol>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="6">
+                <v-btn
+                  color="primary"
+                  href="./w3champions-setup.exe"
+                  target="_blank"
+                  value
+                  outlined
+                  class="w3-background"
+                >
+                  <v-icon>mdi-download</v-icon>
+                  <span class="mr-2 hidden-xs-only">Windows installer</span>
+                </v-btn>
+
                 <p>
-                  If you like give it a try yourself, check out #setup-guide. Testing and feedback are highly appreciated.
-                  Missing features:
+                  <b>Issues with the download link?</b>
+                  <br />Some Antivir-Software has falsely detected our setup as malicious. We contacted them and some already corrected their databases. If thats not the case for you, use the following link and unpack the setup with the
+                  <b style="white-space: nowrap;">password WC3</b>
                 </p>
-                <ul>
-                  <li>multiple maps</li>
-                  <li>map vetos</li>
-                  <li>MMR</li>
-                  <li>Ladder</li>
-                </ul>I assume this can be archived until the end of the week - depending on how much spare time i can invest this week.
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
+                <v-btn
+                  class="w3-background"
+                  color="secondary"
+                  href="./w3champions-setup.zip"
+                  target="_blank"
+                  value
+                  outlined
+                >
+                  <v-icon>mdi-download</v-icon>
+                  <span class="mr-2 hidden-xs-only">Windows installer zipped</span>
+                </v-btn>
+              </v-col>
+              <v-col cols="6">
+                <v-btn
+                  class="w3-background"
+                  color="primary"
+                  href="./webui.zip"
+                  target="_blank"
+                  outlined
+                >
+                  <v-icon>mdi-download</v-icon>
+                  <span class="mr-2 hidden-xs-only">Mac archive</span>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
       </v-col>
       <v-col cols="12" md="4">
         <v-row>
