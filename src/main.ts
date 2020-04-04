91b3f6a1d5d0a97915a29e26d36c8d0fcec76a0f
Nap <napnap@protonmail.com>
2020-04-04 16:24:34 +0200

    update API basepath

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,13 @@ Vue.config.productionTip = false;
 
 let _apiUrl =
   process.env.NODE_ENV === "development"
-    ? "https://test-api.w3champions.com"
+    ? "http://176.28.16.249:10200/"
     : "https://api.w3champions.com";
 
 if ((window as any)._env_.STAGE) {
   _apiUrl =
     (window as any)._env_.STAGE === "dev"
-      ? "https://test-api.w3champions.com"
+      ? "http://176.28.16.249:10200/"
       : "https://api.w3champions.com";
 }
 
