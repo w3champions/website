c4845521fd55c7fb260ba7cf0fe40457fc0c8649
Simon Heiss <simonheiss87@gmail.com>
2020-04-05 16:53:03 +0200

    added tab

--- src/views/OverallStatistics.vue
+++ src/views/OverallStatistics.vue
@@ -1,5 +1,22 @@
 <template>
-  <span>Hallo neue Seite</span>
+  <v-container class="profile">
+    <v-row>
+      <v-col cols="12">
+        <v-card tile>
+          <v-card-title>
+            <span>Statistics of W3Champions</span>
+          </v-card-title>
+          <v-tabs>
+            <v-tabs-slider />
+            <v-tab class="profileTab" :href="`#tab-1`">Games Per Day</v-tab>
+            <v-tab-item :value="'tab-1'">
+              <v-card-title>Games Per Day</v-card-title>
+            </v-tab-item>
+          </v-tabs>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
