7cbd8fee3ab3b7bbe3c43cb2d27654dbfc66b1d5
Simon Heiss <simonheiss87@gmail.com>
2020-04-05 23:35:53 +0200

    added race vs race stats

--- src/views/OverallStatistics.vue
+++ src/views/OverallStatistics.vue
@@ -13,20 +13,35 @@
             <v-tab class="profileTab" :href="`#tab-3`">Winrates</v-tab>
             <v-tab-item :value="'tab-1'">
               <v-card-text v-if="!loadingGamesPerDayStats">
-                <amount-per-day-chart class="ammount-per-day-chart" :game-days="gameDays" />
+                <amount-per-day-chart
+                  class="ammount-per-day-chart"
+                  :game-days="gameDays"
+                />
               </v-card-text>
             </v-tab-item>
             <v-tab-item :value="'tab-2'">
               <v-card-text v-if="!loadingPlayersPerDayStats">
-                <amount-per-day-chart class="ammount-per-day-chart" :game-days="playersPerDay" />
+                <amount-per-day-chart
+                  class="ammount-per-day-chart"
+                  :game-days="playersPerDay"
+                />
               </v-card-text>
             </v-tab-item>
             <v-tab-item :value="'tab-3'">
               <v-card-text v-if="!loadingMapAndRaceStats">
-                <span>Games in 1v1{{
-                    statsPerRaceAndMap.statsPerModes[0].totalGames
-                  }}</span
-                >
+                <v-data-table hide-default-footer :headers="headers" :items="raceWinrateWithoutRandom">
+                  <template v-slot:body="{ items }">
+                    <tbody>
+                      <tr v-for="item in items" :key="item.race">
+                        <td>{{ $t("races." + raceEnums[item.race]) }}</td>
+                        <td>{{ winrateText(item.winLosses[1]) }}</td>
+                        <td>{{ winrateText(item.winLosses[2]) }}</td>
+                        <td>{{ winrateText(item.winLosses[3]) }}</td>
+                        <td>{{ winrateText(item.winLosses[4]) }}</td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-data-table>
               </v-card-text>
             </v-tab-item>
           </v-tabs>
@@ -40,12 +55,21 @@
 import AmountPerDayChart from "@/components/AmountPerDayChart.vue";
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
-import { GameDay, StatsPerMapAndRace } from "@/store/overallStats/types";
+import { GameDay, StatsPerMapAndRace, WinLoss } from "@/store/overallStats/types";
+import { ERaceEnum } from "@/store/typings";
 
 @Component({
-  components: { AmountPerDayChart }
+  components: {
+    AmountPerDayChart
+  }
 })
 export default class OverallStatisticsView extends Vue {
+  public raceEnums = ERaceEnum;
+
+  public winrateText(item: WinLoss) {
+    return `${(item.winrate * 100).toFixed(1)}% (${item.wins}/${item.losses})`;
+  }
+
   get loadingMapAndRaceStats(): boolean {
     return this.$store.direct.state.overallStatistics.loadingMapAndRaceStats;
   }
@@ -70,6 +94,10 @@ export default class OverallStatisticsView extends Vue {
     return this.$store.direct.state.overallStatistics.statsPerMapAndRace;
   }
 
+  get raceWinrateWithoutRandom() {
+    return this.statsPerRaceAndMap.statsPerModes[0].raceVersusRaceRatio.raceWinRatio.slice(1, 5);
+  }
+
   mounted() {
     this.init();
   }
@@ -79,6 +107,38 @@ export default class OverallStatisticsView extends Vue {
     await this.$store.direct.dispatch.overallStatistics.loadPlayersPerDayStatistics();
     await this.$store.direct.dispatch.overallStatistics.loadMapAndRaceStatistics();
   }
+  public headers = [
+    {
+      text: "",
+      align: "start",
+      sortable: false,
+      value: "race"
+    },
+    {
+      text: `VS Human`,
+      align: "start",
+      sortable: false,
+      value: "vsHu"
+    },
+    {
+      text: "VS Orc",
+      align: "start",
+      sortable: false,
+      value: "vsOc"
+    },
+    {
+      text: "VS Undead",
+      align: "start",
+      sortable: false,
+      value: "vsUd"
+    },
+    {
+      text: "VS Night Elve",
+      align: "start",
+      sortable: false,
+      value: "vsNe"
+    }
+  ];
 }
 </script>
 
