6d3d0fb74fe2e8eaef5e6e306def53fd2322cddc
Simon Heiss <simonheiss87@gmail.com>
2020-04-07 15:04:03 +0200

    added team game info

---
+++ src/components/TeamMatchInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <player-match-info :player="team.players[0]" :left="left" />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { Team } from "@/store/typings";
+import PlayerMatchInfo from "@/components/PlayerMatchInfo.vue";
+
+@Component({
+  components: { PlayerMatchInfo }
+})
+export default class TeamMatchInfo extends Vue {
+  @Prop() public team!: Team;
+  @Prop() public left!: boolean;
+}
+</script>
