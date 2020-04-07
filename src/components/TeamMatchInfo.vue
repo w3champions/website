4c4405878ae8e5d5699076b9fbba1f65e22eb91f
Simon Heiss <simonheiss87@gmail.com>
2020-04-07 15:28:20 +0200

    4v4, 2v2 fine ffa working, i guess

--- src/components/TeamMatchInfo.vue
+++ src/components/TeamMatchInfo.vue
@@ -1,5 +1,10 @@
 <template>
-  <player-match-info :player="team.players[0]" :left="left" />
+  <div>
+    <player-match-info :player="team.players[0]" :left="left" />
+    <player-match-info v-if="team.players.length > 1" :player="team.players[1]" :left="left" />
+    <player-match-info v-if="team.players.length > 2" :player="team.players[2]" :left="left" />
+    <player-match-info v-if="team.players.length > 3" :player="team.players[3]" :left="left" />
+  </div>
 </template>
 
 <script lang="ts">
