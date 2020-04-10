20dcdb69627f2f1b3423495e9976f55db56be4d4
Simon Heiss <simonheiss87@gmail.com>
2020-04-10 13:06:23 +0200

    new working webpack debug mode

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   productionSourceMap: false,
-  configureWebpack: {
-    devtool: "#source-map"
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "development") {
+      config.devtool = "eval-source-map";
+      config.output.devtoolModuleFilenameTemplate = info =>
+        info.resourcePath.match(/\.vue$/) &&
+        !info.identifier.match(/type=script/) // this is change ✨
+          ? `webpack-generated:///${info.resourcePath}?${info.hash}`
+          : `webpack-yourCode:///${info.resourcePath}`;
+
+      config.output.devtoolFallbackModuleFilenameTemplate =
+        "webpack:///[resource-path]?[hash]";
+    }
   }
 };
