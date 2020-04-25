31727ca38f38bbcd27e4a6357f985662935a77b0
Simon Heiss <simonheiss87@gmail.com>
2020-04-25 15:15:27 +0200

    header with names and rating working

--- src/views/MatchDetail.vue
+++ src/views/MatchDetail.vue
@@ -4,7 +4,9 @@
       <v-col cols="12">
         <v-card tile>
           <v-card-title>
-            Grubby VS OtherDude
+            <span class="won">{{ winnerNames }} +{{winnerMmrGain}}</span>
+            <span style="margin-left: 20px; margin-right: 20px">VS</span>
+            <span class="lost">{{ looserNames }} {{looserMmrGain}}</span>
           </v-card-title>
           <v-tabs>
             <v-tabs-slider></v-tabs-slider>
@@ -12,7 +14,6 @@
             <v-tab class="profileTab" :href="`#tab-heroes`">Heroes</v-tab>
 
             <v-tab-item :value="'tab-overall'">
-              {{ playerDetails.length }}
             </v-tab-item>
             <v-tab-item :value="'tab-heroes'">
 
@@ -41,8 +42,28 @@ export default class MatchDetailView extends Vue {
     this.init();
   }
 
+  get winnerNames() {
+    return this.matchResult.teams[0].players.map(p => `${p.name} (${p.oldMmr})`).join(", ");
+  }
+
+  get looserNames() {
+    return this.matchResult.teams[1].players.map(p => `${p.name} (${p.oldMmr})`).join(", ");
+  }
+
+  get winnerMmrGain() {
+    return this.matchResult.teams[0].players[0].mmrGain;
+  }
+
+  get looserMmrGain() {
+    return this.matchResult.teams[1].players[0].mmrGain;
+  }
+
   get playerDetails() {
-    return this.$store.direct.state.match.matchDetail?.playerScores;
+    return this.$store.direct.state.match.matchDetail.playerScores;
+  }
+
+  get matchResult() {
+    return this.$store.direct.state.match.matchDetail.match;
   }
 
   get loading() {
