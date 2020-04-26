80c8bbbe76a49aba645042bdfb8beff891fd9ab6
Simon Heiss <simonheiss87@gmail.com>
2020-04-26 12:29:51 +0200

    fixed error in barchart

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -9,37 +9,40 @@ import { ChartData } from "chart.js";
 })
 export default class BarChart extends Mixins(Bar) {
   @Prop() public chartData!: ChartData;
-
-  mounted() {
-    this.renderChart(this.chartData, {
-      legend: {
-        display: false
+  private options = {
+    legend: {
+      display: false
+    },
+    tooltips: {
+      custom: function(tooltip) {
+        if (!tooltip) return;
+        tooltip.displayColors = false;
       },
-      tooltips: {
-        custom: function(tooltip) {
-          if (!tooltip) return;
-          tooltip.displayColors = false;
+      callbacks: {
+        label: function(tooltipItem) {
+          return `${tooltipItem.xLabel} - ${tooltipItem.yLabel}`;
         },
-        callbacks: {
-          label: function(tooltipItem) {
-            return `${tooltipItem.xLabel} - ${tooltipItem.yLabel}`;
-          },
-          title: function() {
-            return "";
+        title: function() {
+          return "";
+        }
+      }
+    },
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
           }
         }
-      },
-      maintainAspectRatio: false,
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true
-            }
-          }
-        ]
-      }
-    });
+      ]
+    }
+  };
+
+  mounted() {
+    if (this.chartData) {
+      this.renderChart(this.chartData, this.options);
+    }
   }
 }
 </script>
