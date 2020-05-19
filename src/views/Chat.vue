43fcfe13ffb981380300dfb02216046f3b33719d
Simon Heiss <simonheiss87@gmail.com>
2020-05-19 23:43:54 +0200

    name now shown

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,20 +2,20 @@
   <v-row>
     <v-col cols="1"/>
     <v-col cols="7">
-      <v-card>
+      <v-card class="pt-4">
         <div class="overflow-y-auto" style="height: 600px" id="chatBox">
           <v-list-item style="min-height: 25px !important;" v-for="message in messages" :key="message.index">
-            {{ message.user }}: {{ message.message }}
+            {{ message.name }}: {{ message.message }}
           </v-list-item>
         </div>
         <v-text-field autofocus v-model="editChatMessage" @keydown.enter="sendMessage" :disabled="canNotSend" />
       </v-card>
     </v-col>
     <v-col cols="3">
-      <v-card>
+      <v-card class="pt-4">
         <div class="overflow-y-auto" style="height: 600px">
-          <v-list-item style="min-height: 25px !important;" v-for="user in users" :key="user.battleTag">
-            {{ user.user }}
+          <v-list-item style="min-height: 25px !important;" v-for="user in users" :key="user.battleTag" @click="openProfile(user.battleTag)">
+            {{ user.name }}
           </v-list-item>
         </div>
       </v-card>
@@ -33,36 +33,43 @@ export default class ChatView extends Vue {
   public connection!: HubConnection;
   public editChatMessage = "connecting...";
   public canNotSend = true;
-  public messages = [] as { user: string, message: string, index: number }[]
+  public messages = [] as { user: string, name: string, message: string, index: number }[]
   public users = [] as { name: string, battleTag: string }[]
 
 
   async mounted() {
     this.connection = new HubConnectionBuilder().withUrl(`${API_URL}chathub`).build()
     await this.connection.start();
-    this.canNotSend = false
-    this.editChatMessage = ""
 
-    this.connection.on("StartChat", this.pushUsers);
+    this.connection.on("StartChat", this.pushInitialUsers);
     this.connection.on("ReceiveMessage", this.pushMessage);
     this.connection.on("UserEntered", this.pushUser);
     this.connection.on("UserLeft", this.removeUser);
     this.connection.onclose(this.handleClose);
+
+    await this.connection.invoke("LoginAs", this.authCode);
+
+    this.canNotSend = false
+    this.editChatMessage = ""
   }
 
   get authCode(): string {
     return this.$store.direct.state.oauth.token;
   }
 
-  public pushMessage(user: string, message: string) {
-    this.messages.push({ user, message, index: this.messages.length })
+  public openProfile(battleTag: string) {
+    this.$router.push({ path: `/player/${encodeURIComponent(battleTag)}` });
+  }
+
+  public pushMessage(user: string, name:string, message: string) {
+    this.messages.push({ user, name, message, index: this.messages.length })
   }
 
   public pushUser(name: string, battleTag: string) {
     this.users.push({ name, battleTag })
   }
 
-  public pushUsers(users: {name: string, battleTag: string}[]) {
+  public pushInitialUsers(users: {name: string, battleTag: string}[]) {
     this.users = users;
   }
 
