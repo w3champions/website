2130542ca3bd397de87d6523689d7fd70d15127a
lennartblom <lennart@blom.de>
2020-06-21 13:03:41 +0200

    Add race icons to player views
    • Player Stats Race Versus Race on Map
    • Stats per race
    
    • Optimize icons in to 128px size
    • Move "as" string to tooltip

---
+++ src/components/player/RaceIcon.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <img v-if="race!==raceEnums.TOTAL" v-bind:src="renderIcon()" v-bind:title="enumToString()" class="race-icon" height="24px" width="auto"/>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import { ERaceEnum } from "@/store/typings";
+    import { Component, Prop } from "vue-property-decorator";
+
+    @Component({})
+    export default class RaceIcon extends Vue {
+
+        public raceEnums = ERaceEnum;
+        @Prop() race!: ERaceEnum;
+
+        enumToString() {
+            return ERaceEnum[this.race];
+        }
+
+        renderIcon() {
+            return require("../../assets/raceIcons/" + this.enumToString() + ".png");
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
