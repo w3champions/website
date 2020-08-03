bfcc3c8fd01589ae836754f310d51ddfea3a0acf
Simon Heiss <simonheiss87@gmail.com>
2020-08-03 18:52:04 +0200

    add launcher TAB

--- env.sh
+++ env.sh
@@ -4,5 +4,7 @@ rm ./env.js
 
 echo "window._env_ = {" >> ./env.js
 echo "  BASE_URL: \"${1}\","  >> ./env.js
-echo "  REDIRECT_URL: \"${2}\""  >> ./env.js
+echo "  REDIRECT_URL: \"${2}\","  >> ./env.js
+echo "  IS_LAUNCHER_ENABLED: \"${3}\","  >> ./env.js
+echo "  LAUNCHER_UPDATE_URL: \"${4}\""  >> ./env.js
 echo "};" >> ./env.js
