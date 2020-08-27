74d62fe804ae4c6379231bcf51f49b4c4564a4cd
Simon Heiss <simonheiss87@gmail.com>
2020-08-27 11:12:48 +0200

    commented out badges, fixed 16:9 ratio for mac aswell

--- src/views/GettingStarted.vue
+++ src/views/GettingStarted.vue
@@ -244,7 +244,9 @@
                       </li>
                     </ul>
                   </p>
-                  <iframe class="mt-3" width="100%" height="315" src="https://www.youtube.com/embed/8s53BHfKPLs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <div class="mt-10 video-container">
+                    <iframe src="https://www.youtube.com/embed/8s53BHfKPLs" frameborder="0" allowfullscreen></iframe>
+                  </div>
                 </v-card-text>
               </v-tab-item>
             </v-tabs-items>
@@ -347,4 +349,18 @@ export default class GettingStartedView extends Vue {
   height: 100px;
 }
 
+.video-container {
+  position: relative;
+  padding-bottom: 62.25%;
+  height: 0;
+  overflow: hidden;
+}
+
+.video-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>
