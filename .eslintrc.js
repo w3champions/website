dc3832e9f7674b45eb6751c645b6534234686f92
Alexander Beninski <a.beninski@gmail.com>
2020-09-20 13:17:05 +0300

    mode stats grid improvements (#173)

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,11 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "ban-ts-comment": "off",
     camelcase: "off",
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
