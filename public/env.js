7935a54a8de4f6deead3427b38ca2f7423fb36b5
weidenbach <73471359+weidenbach@users.noreply.github.com>
2020-11-26 11:54:08 +0100

    Update env.js

--- public/env.js
+++ public/env.js
@@ -5,7 +5,5 @@ window._env_ = {
   REDIRECT_URL: "http://localhost:8080/login",
   LAUNCHER_UPDATE_URL: "https://update-service.test.w3champions.com/api/",
   INGAME_STATIC_RESOURCES_URL: "https://storage.googleapis.com/w3champions-prod/integration/",
-  // BNET_API_CLIENT_ID: '6da84149578f4566a4d0224a2264a54d',
-  
   BNET_API_CLIENT_ID: "d7bd6dd46e2842c8a680866759ad34c2",
 };
