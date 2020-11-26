cf03630edc689f01d80f81be4c16364a6c2e4688
weidenbach <73471359+weidenbach@users.noreply.github.com>
2020-11-26 11:53:34 +0100

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,11 +1,11 @@
 window._env_ = {
-  BASE_URL: "http://localhost:5000/",
-  // BASE_URL: "https://statistic-service.w3champions.com/",
+  // BASE_URL: "http://localhost:5000/",
+  BASE_URL: "https://statistic-service.w3champions.com/",
   // BASE_URL: "https://statistic-service-test.w3champions.com/",
   REDIRECT_URL: "http://localhost:8080/login",
   LAUNCHER_UPDATE_URL: "https://update-service.test.w3champions.com/api/",
-  INGAME_STATIC_RESOURCES_URL:
-    "https://storage.googleapis.com/w3champions-prod/integration/",
+  INGAME_STATIC_RESOURCES_URL: "https://storage.googleapis.com/w3champions-prod/integration/",
   // BNET_API_CLIENT_ID: '6da84149578f4566a4d0224a2264a54d',
+  
   BNET_API_CLIENT_ID: "d7bd6dd46e2842c8a680866759ad34c2",
 };
