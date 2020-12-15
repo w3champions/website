7a50ef6087f51a571cd386157a989b86a3120d4c
Aaron Jewell <ajewell@getacceptd.com>
2020-12-15 13:22:20 -0500

    Add tiptap wysiwyg to admin page.
    Version 2 of tiptap will provide proper typings for the components.
    For now, relying no some community submitted typing. Still need
    to explore binding the wysiwyg content to the component/store
    and handling image uploads to a CDN.

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -120,7 +120,7 @@
             <v-toolbar flat color="transparent">
               <v-toolbar-title>News for Launcher</v-toolbar-title>
               <v-spacer></v-spacer>
-              <v-dialog v-model="dialogNews">
+              <v-dialog v-model="dialogNews" max-width="50rem">
                 <template v-slot:activator="{ on, attrs }">
                   <v-btn color="primary" class="mb-2" v-bind="attrs" v-on="on">
                     Add News
@@ -132,20 +132,150 @@
                   </v-card-title>
 
                   <v-card-text>
-                    <v-container>
-                      <v-text-field
-                        v-model="editedNewsItem.date"
-                        filled
-                        label="Headline"
+                    <div class="editor">
+                      <editor-menu-bar
+                        :editor="editor"
+                        v-slot="{ commands, isActive }"
+                      >
+                        <div class="menubar">
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.bold() }"
+                            @click="commands.bold"
+                          >
+                            <v-icon>mdi-format-bold</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.italic() }"
+                            @click="commands.italic"
+                          >
+                            <v-icon>mdi-format-italic</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.strike() }"
+                            @click="commands.strike"
+                          >
+                            <v-icon>mdi-format-strikethrough</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.underline() }"
+                            @click="commands.underline"
+                          >
+                            <v-icon>mdi-format-underline</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.code() }"
+                            @click="commands.code"
+                          >
+                            <v-icon>mdi-code-tags</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.paragraph() }"
+                            @click="commands.paragraph"
+                          >
+                            <v-icon>mdi-format-paragraph</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{
+                              'is-active': isActive.heading({ level: 1 }),
+                            }"
+                            @click="commands.heading({ level: 1 })"
+                          >
+                            <v-icon>mdi-format-header-1</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{
+                              'is-active': isActive.heading({ level: 2 }),
+                            }"
+                            @click="commands.heading({ level: 2 })"
+                          >
+                            <v-icon>mdi-format-header-2</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{
+                              'is-active': isActive.heading({ level: 3 }),
+                            }"
+                            @click="commands.heading({ level: 3 })"
+                          >
+                            <v-icon>mdi-format-header-3</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.bullet_list() }"
+                            @click="commands.bullet_list"
+                          >
+                            <v-icon>mdi-format-list-bulleted</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.ordered_list() }"
+                            @click="commands.ordered_list"
+                          >
+                            <v-icon>mdi-format-list-numbered</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.blockquote() }"
+                            @click="commands.blockquote"
+                          >
+                            <v-icon>mdi-format-quote-close</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            :class="{ 'is-active': isActive.code_block() }"
+                            @click="commands.code_block"
+                          >
+                            <v-icon>mdi-code-tags</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            @click="commands.horizontal_rule"
+                          >
+                            hr
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            @click="commands.undo"
+                          >
+                            <v-icon>mdi-undo</v-icon>
+                          </button>
+
+                          <button
+                            class="menubar__button"
+                            @click="commands.redo"
+                          >
+                            <v-icon>mdi-redo</v-icon>
+                          </button>
+                        </div>
+                      </editor-menu-bar>
+
+                      <editor-content
+                        class="editor__content"
+                        :editor="editor"
                       />
-                      <v-textarea
-                        auto-grow
-                        filled
-                        rows="1"
-                        v-model="editedNewsItem.message"
-                        label="Message"
-                      />
-                    </v-container>
+                    </div>
                   </v-card-text>
 
                   <v-card-actions>
@@ -234,15 +364,78 @@ import store from "@/store";
 import moment from "moment";
 import Vue from "vue";
 import { Component, Watch } from "vue-property-decorator";
+import { Editor, EditorContent, EditorMenuBar } from "tiptap"
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+} from "tiptap-extensions"
 import {
   BannedPlayer,
   LoadingScreenTip,
   NewsMessage,
 } from "../store/admin/types";
-@Component({ components: {} })
+@Component({ components: { EditorContent, EditorMenuBar } })
 export default class Admin extends Vue {
   data() {
     return {
+      editor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new HorizontalRule(),
+          new ListItem(),
+          new OrderedList(),
+          new TodoItem(),
+          new TodoList(),
+          new Link(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Strike(),
+          new Underline(),
+          new History(),
+        ],
+        content: `
+          <h2>
+            Hi there,
+          </h2>
+          <p>
+            this is a very <em>basic</em> example of tiptap.
+          </p>
+          <pre><code>body { display: none; }</code></pre>
+          <ul>
+            <li>
+              A regular list
+            </li>
+            <li>
+              With regular items
+            </li>
+          </ul>
+          <blockquote>
+            It's amazing 👏
+            <br />
+            – mom
+          </blockquote>
+        `,
+      }),
       headers: [
         {
           text: "BattleTag",
