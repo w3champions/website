6dbeb78787538aa16cbcc5a12b0488b77595fbfb
Simon Heiss <simonheiss87@gmail.com>
2021-01-11 17:36:37 +0100

    redirection now dependant on base url

--- public/env.js
+++ public/env.js
@@ -4,7 +4,6 @@ window._env_ = {
   BASE_URL: "https://statistic-service.test.w3champions.com/",
   // IDENTIFICATION_URL: "http://localhost:5000/",
   IDENTIFICATION_URL: "https://identification-service.test.w3champions.com/",
-  REDIRECT_URL: "http://localhost:8080/login",
   LAUNCHER_UPDATE_URL: "https://update-service.test.w3champions.com/api/",
   INGAME_STATIC_RESOURCES_URL: "https://storage.googleapis.com/w3champions-prod/integration/",
   BNET_API_CLIENT_ID: "d7bd6dd46e2842c8a680866759ad34c2",
