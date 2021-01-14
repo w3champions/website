6bd83d19567976244d616f47f99e8da665edcb0e
Marwan Zibaoui <marwan@wunderflats.com>
2021-01-14 07:24:15 +0100

    clans/AcceptInvitePanel

--- src/components/clans/AcceptInvitePanel.vue
+++ src/components/clans/AcceptInvitePanel.vue
@@ -3,19 +3,24 @@
     <v-row class="justify-center">
       <v-col class="text-center">
         <h3>
-          You were invited by
+          {{ $t("components_clans_acceptinvitepanel.invitemsg1") }}
           <b>{{ clanName }}</b>
-          to join their ranks!
+          {{ $t("components_clans_acceptinvitepanel.invitemsg2") }}
         </h3>
       </v-col>
     </v-row>
 
     <v-row class="justify-center">
       <v-col cols="3" class="text-center">
-        <v-btn @click="joinClan">Join {{ clanName }} !</v-btn>
+        <v-btn @click="joinClan">
+          {{ $t("components_clans_acceptinvitepanel.join") }}
+          {{ clanName }} !
+        </v-btn>
       </v-col>
       <v-col cols="3" class="text-center">
-        <v-btn outlined @click="rejectClan">Reject invite</v-btn>
+        <v-btn outlined @click="rejectClan">
+          {{ $t("components_clans_acceptinvitepanel.reject") }}
+        </v-btn>
       </v-col>
     </v-row>
   </v-card-text>
