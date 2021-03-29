b1376f9e941e352254cd2e80b9fcc267e77bf1c3
John Graves <johntaylorgraves@gmail.com>
2021-03-30 00:10:46 +0100

    crypto dialog boxes WIP

--- src/components/common/SupportBox.vue
+++ src/components/common/SupportBox.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card class="support-card" tile>
     <v-card-text class="text-center text-primary" style="opacity: 0.87">
-      Support us at:
+      Support us:
     </v-card-text>
 
     <!-- PATREON -->
@@ -14,6 +14,26 @@
         <v-img src="../../../src/assets/socials/PayPal_button.png" />
     </v-card>
 
+    <!-- BTC -->
+    <v-card class="support-subcard" tile outlined>
+      <template>
+        <div @click.stop="openCryptoDialog = true">
+          <v-img src="../../../src/assets/socials/Bitcoin_button.png" />
+          <crypto-dialog :Crypto="`BTC`"></crypto-dialog>
+        </div>
+      </template>
+    </v-card>
+
+    <!-- ETH -->
+    <v-card class="support-subcard" href="https://www.paypal.me/w3champions" tile outlined>
+        <v-img src="../../../src/assets/socials/Ethereum_button.png" />
+    </v-card>
+
+    <!-- LTC -->
+    <v-card class="support-subcard" href="https://www.paypal.me/w3champions" tile outlined>
+        <v-img src="../../../src/assets/socials/Litecoin_button.png" />
+    </v-card>
+
     <!-- MATCHERINO - TBD -->
   </v-card>
 </template>
@@ -21,6 +41,7 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
+import CryptoDialog from "@/components/common/CryptoDialog.vue"
 
 // In the long term, I want the current matcherino link for the
 // latest end-of-season tournament to be auto-updated here.
@@ -28,14 +49,28 @@ import { Component, Prop } from "vue-property-decorator";
 // this would require adding the link to the state management
 // then checking the latest season.
 
-@Component({})
+@Component({ components: { CryptoDialog } })
 export default class SupportBox extends Vue {
   @Prop() title!: string;
   @Prop() text!: string;
 
+  SupportBox() : void {
+    this.openCryptoDialog = false;
+  }
+
+  set openCryptoDialog(value : boolean) {
+    this.openCryptoDialog = value;
+  };
+
+  get openCryptoDialog() : boolean {
+    return this.openCryptoDialog;
+  }
+
   get patreon(): string {
     return "https://www.patreon.com/w3champions";
   }
+
+  
 }
 </script>
 
