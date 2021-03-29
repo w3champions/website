b74531a5175491f5c1be1092ede35a63bc15ebf3
John Graves <johntaylorgraves@gmail.com>
2021-03-30 00:10:46 +0100

    crypto dialog boxes WIP

---
+++ src/components/common/CryptoDialog.vue
@@ -0,0 +1,20 @@
+<template>
+<v-dialog>
+    <v-card>
+        <v-card-title>
+            Hi this is a dialog
+        </v-card-title>
+    </v-card>
+</v-dialog>
+    
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class CryptoDialog extends Vue {
+    @Prop() Crypto! : string;
+}
+</script>
