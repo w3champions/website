f2836f2d2c674256f3bf2e986e529a84522c9b81
Ole <jan-ole@berger-mail.de>
2021-09-21 21:24:04 +0200

    adding fr

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 const constants = {
   ongoingMatchesRefreshInterval: 20000,
   queueDataRefreshInterval: 10000,
-  activeLanguages: ["en", "ru", "kr", "zh", "de", "pl", "ua", "pt"],
+  activeLanguages: ["en", "ru", "kr", "zh", "de", "pl", "ua", "pt", "fr"],
 };
 export default constants;
