cd85e30c426af9ecedb771f25bcc7467c1a7bf57
John Graves <johntaylorgraves@gmail.com>
2022-07-25 19:01:20 +0100

    [W3C-93] bust cache

--- src/helpers/url-functions.ts
+++ src/helpers/url-functions.ts
@@ -7,22 +7,22 @@ export function getAvatarUrl(
   isClassic: boolean
 ) {
   if (picId == undefined || picId == null) {
-    return `${INGAME_STATIC_RESOURCES_URL}icons/raceAvatars/STARTER_${Math.floor(Math.random() * 5) + 1}.jpg`;
+    return `${INGAME_STATIC_RESOURCES_URL}icons/raceAvatars/STARTER_${Math.floor(Math.random() * 5) + 1}.jpg?v=2`;
   }
 
   const raceIconPrefix = isClassic ? "classic/" : "";
 
   if (category == EAvatarCategory.STARTER) {
-    return `${INGAME_STATIC_RESOURCES_URL}icons/raceAvatars/${raceIconPrefix}STARTER_${picId}.jpg`;
+    return `${INGAME_STATIC_RESOURCES_URL}icons/raceAvatars/${raceIconPrefix}STARTER_${picId}.jpg?v=2`;
   }
 
   if (category == EAvatarCategory.SPECIAL) {
-    return `${INGAME_STATIC_RESOURCES_URL}icons/specialAvatars/SPECIAL_${picId}.jpg`;
+    return `${INGAME_STATIC_RESOURCES_URL}icons/specialAvatars/SPECIAL_${picId}.jpg?v=2`;
   }
 
   const categoryString = EAvatarCategory[category].toString();
 
-  return `${INGAME_STATIC_RESOURCES_URL}icons/raceAvatars/${raceIconPrefix}${categoryString}_${picId}.jpg`;
+  return `${INGAME_STATIC_RESOURCES_URL}icons/raceAvatars/${raceIconPrefix}${categoryString}_${picId}.jpg?v=2`;
 }
 
 export function getProfileUrl(playerId: string) {
