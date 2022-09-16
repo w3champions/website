2259c57cc5e776d8ff76397d596871e0cccd49b3
George Marcus <george.marcus922@gmail.com>
2022-09-17 00:55:32 +0300

    Commit new localized string

--- src/locales/data.ts
+++ src/locales/data.ts
@@ -326,6 +326,7 @@ const data = {
       vsorc: "vs Orc",
       vsud: "vs Undead",
       vsne: "vs Night Elf",
+      vsrdm: "vs Random",
     },
     "components_overall-statistics_gamelengthchart": {
       amountofgames: "amount of games",
