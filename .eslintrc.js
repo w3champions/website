c2d94ed230eb9cfc0584d262845b3e2ecb185207
Gustav Andersson <gustav87and@gmail.com>
2023-01-15 13:20:30 +0100

    Deactivate prettier and add some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,37 @@ module.exports = {
   env: {
     node: true,
   },
-  //plugins: [ 
+  //plugins: [
   //  "prettier"
   //],
   extends: [
     "eslint:recommended",
     "plugin:vue/essential",
-    "plugin:prettier/recommended",
+    //  "plugin:prettier/recommended",
     "@vue/typescript/recommended",
-    //"@vue/prettier/@typescript-eslint"
-    //"@vue/typescript/recommended",
-    //"@vue/prettier/@typescript-eslint",
+    // "@vue/prettier/@typescript-eslint"
+    // "@vue/typescript/recommended",
+    // "@vue/prettier/@typescript-eslint",
   ],
   parser: "vue-eslint-parser",
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "prettier/prettier": "warn",
+    // "prettier/prettier": "warn",
+    "semi": "off",
+    "@typescript-eslint/semi": "warn",
+    "@typescript-eslint/member-delimiter-style": "warn",
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": ["warn", "only-multiline"],
+    "quotes": "off",
+    "@typescript-eslint/quotes": ["warn", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
+    "object-curly-spacing": ["warn", "always"],
+    "space-infix-ops": "off",
+    "@typescript-eslint/space-infix-ops": "warn",
+    "no-trailing-spaces": "warn",
+    "arrow-parens": ["warn", "always"],
+    "@typescript-eslint/no-non-null-assertion": "off",
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "prefer-const": "warn",
