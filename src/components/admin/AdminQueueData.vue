848dcfeee3dc34e99add3c2e07b35fbc8a4e02e7
Gustav Andersson <gustav87and@gmail.com>
2023-01-26 11:43:02 +0100

    Use new game-modes mixin (#586)

--- src/components/admin/AdminQueueData.vue
+++ src/components/admin/AdminQueueData.vue
@@ -9,14 +9,14 @@
             :key="index"
           >
             <v-expansion-panel-header>
-              {{ mode.text }}
+              {{ mode.name }}
             </v-expansion-panel-header>
             <v-expansion-panel-content>
               <template>
-                <div v-if="getPlayerDataInGamemode(mode.modeId) != null">
+                <div v-if="getPlayerDataInGamemode(mode.id) != null">
                   <v-data-table
                     :headers="headers"
-                    :items="getPlayerDataInGamemode(mode.modeId)"
+                    :items="getPlayerDataInGamemode(mode.id)"
                     :items-per-page="-1"
                     :disable-pagination="true"
                     :hide-default-footer="true"
@@ -34,13 +34,15 @@
 
 <script lang="ts">
 import { QueueData } from "@/store/admin/types";
-import Vue from "vue";
-import { Component, Watch } from "vue-property-decorator";
+import GameModesMixin from "@/mixins/GameModesMixin";
 import { EGameMode } from "@/store/typings";
+import { Component, Watch, Mixins, Prop } from "vue-property-decorator";
+import { LocaleMessage } from "vue-i18n";
 import AppConstants from "@/constants";
 
 @Component({ components: {} })
-export default class AdminQueueData extends Vue {
+export default class AdminQueueData extends Mixins(GameModesMixin) {
+  @Prop() disabledModes?: EGameMode[];
   _intervalRefreshHandle?: number = undefined;
 
   get headers(): Array<unknown> {
@@ -108,6 +110,8 @@ export default class AdminQueueData extends Vue {
   }
 
   private async init(): Promise<void> {
+    await this.loadActiveGameModes();
+
     if (this.isAdmin) {
       await this.$store.direct.dispatch.admin.loadQueueData(
         this.$store.direct.state.oauth.token
@@ -129,55 +133,6 @@ export default class AdminQueueData extends Vue {
     return this.$store.direct.state.oauth.isAdmin;
   }
 
-  get gameModes(): Array<unknown> {
-    return [
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_1ON1]}`),
-        modeId: EGameMode.GM_1ON1,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_2ON2]}`),
-        modeId: EGameMode.GM_2ON2,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_4ON4]}`),
-        modeId: EGameMode.GM_4ON4,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_FFA]}`),
-        modeId: EGameMode.GM_FFA,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_LEGION_1v1_x20]}`),
-        modeId: EGameMode.GM_LEGION_1v1_x20,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_LEGION_2v2_X20]}`),
-        modeId: EGameMode.GM_LEGION_2v2_X20,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_LEGION_4v4_X20]}`),
-        modeId: EGameMode.GM_LEGION_4v4_X20,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_ROC_1ON1]}`),
-        modeId: EGameMode.GM_ROC_1ON1,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_ATR_1ON1]}`),
-        gameMode: EGameMode.GM_ATR_1ON1,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_BANJOBALL_4ON4]}`),
-        modeId: EGameMode.GM_BANJOBALL_4ON4,
-      },
-      {
-        text: this.$t(`gameModes.${EGameMode[EGameMode.GM_PTR_1ON1]}`),
-        modeId: EGameMode.GM_PTR_1ON1,
-      },
-    ];
-  }
-
   getPlayerDataInGamemode(modeId: number): unknown {
     for (let i = 0; i < this.queueData.length; i++) {
       if (this.queueData[i].gameMode == modeId) {
@@ -186,6 +141,16 @@ export default class AdminQueueData extends Vue {
     }
 
     return null;
+  }
+
+  get gameModes(): Array<{ name: LocaleMessage; id: number }> {
+    let modes = this.activeGameModes;
+
+    if (this.disabledModes) {
+      modes = modes?.filter((x) => !this.disabledModes?.includes(x.id));
+    }
+
+    return modes;
   }
 
   async mounted(): Promise<void> {
