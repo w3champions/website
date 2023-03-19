ed29f984da215c579ad1f033a949f095f464b259
Rotzbua <Rotzbua@users.noreply.github.com>
2023-03-19 16:00:31 +0100

    Refactor `GlobalSearchService` to static service

--- src/services/GlobalSearchService.ts
+++ src/services/GlobalSearchService.ts
@@ -5,7 +5,7 @@ export default class GlobalSearchService {
   // search: the string to search by
   // lastPlayerId: the last id in the currently loaded list (used for pagination)
   // pageSize: the number of players to fetch at a time
-  public async search(search: string, lastPlayerId: string, pageSize: number): Promise<PlayerSearchData[]> {
+  public static async search(search: string, lastPlayerId: string, pageSize: number): Promise<PlayerSearchData[]> {
     let url = `${API_URL}api/players/global-search?search=${encodeURIComponent(search)}&pageSize=${pageSize}`;
     if (lastPlayerId) {
       url += `&lastObjectId=${encodeURIComponent(lastPlayerId)}`;
--- src/store/globalSearch/store.ts
+++ src/store/globalSearch/store.ts
@@ -3,7 +3,6 @@ import { defineStore } from "pinia";
 import GlobalSearchService from "@/services/GlobalSearchService";
 
 const PAGE_SIZE = 20;
-const globalSearchService = new GlobalSearchService();
 
 export const useGlobalSearchStore = defineStore("globalSearch", {
   state: (): GlobalSearchState => ({
@@ -13,7 +12,7 @@ export const useGlobalSearchStore = defineStore("globalSearch", {
   actions: {
     async search(search: { searchText: string; append: boolean }) {
       const lastPlayerId = search.append && this.players.length > 0 ? this.players[this.players.length - 1].battleTag : "";
-      const players = await globalSearchService.search(search.searchText, lastPlayerId, PAGE_SIZE);
+      const players = await GlobalSearchService.search(search.searchText, lastPlayerId, PAGE_SIZE);
 
       this.SET_PLAYERS({ players, append: search.append });
     },
