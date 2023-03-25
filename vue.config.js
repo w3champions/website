593e2aa0bb212d40866c5efba71095af25a9d4ec
Rotzbua <Rotzbua@users.noreply.github.com>
2023-03-25 22:11:05 +0100

    Update package.json
    Update package.json
    
    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 function getPublicPath() {
-  if (process.env.NODE_ENV === "development") {
     return "/";
-  }
-
-  return "SITE_CDN_URL";
 }
 
 /** @type {import('@vue/cli-service').ProjectOptions} */ // This provides type hints for the configuration options below.
