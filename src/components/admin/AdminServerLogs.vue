7eda0165ae2e649cbf711c5181f11481ff6e8d16
Gustav Andersson <gustav87and@gmail.com>
2023-10-13 16:23:39 +0200

    Create admin permissions

---
+++ src/components/admin/AdminServerLogs.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <div>Hello!</div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({ components: {} })
+export default class AdminServerLogs extends Vue {
+
+}
+</script>
+
+<style lang="scss"></style>
