5b70a2c946b20410cd1f82587945cdf99ff0b57f
Gustav Andersson <gustav87and@gmail.com>
2024-04-29 12:04:26 +0200

    use options api instead of composition for SupportBox

--- src/components/common/SupportBox.vue
+++ src/components/common/SupportBox.vue
@@ -29,121 +29,50 @@
 
     <!-- CRYPTOCURRENCIES -->
     <v-card
+      v-for="(crypto, index) in cryptos"
+      v-bind:key="crypto.coin"
       class="support-subcard"
       tile
       outlined
-      @click.stop="setBtcDialogOpen(true)"
+      @click.stop="updateTracker(index, cryptoDialogTracker)"
     >
-      <v-img :src="`/assets/socials/${cryptos()[0].coin}_button.png`" :alt="cryptos()[0].name"></v-img>
+      <v-img :src="`/assets/socials/${crypto.coin}_button.png`" :alt="crypto.name"></v-img>
       <crypto-dialog
-        v-model="btcDialog"
-        :crypto="cryptos()[0].coin"
-        :cryptoName="cryptos()[0].name"
-        :cryptoAddress="cryptos()[0].address"
+        v-model="cryptoDialogTracker[index]"
+        :crypto="crypto.coin"
+        :cryptoName="crypto.name"
+        :cryptoAddress="crypto.address"
       ></crypto-dialog>
     </v-card>
 
+    <!-- Alternates -->
     <v-card
+      v-for="(name, index) in alternates"
+      v-bind:key="index"
       class="support-subcard"
       tile
       outlined
-      @click.stop="setEthDialogOpen(true)"
-    >
-      <v-img :src="`/assets/socials/${cryptos()[1].coin}_button.png`" :alt="cryptos()[1].name"></v-img>
-      <crypto-dialog
-        v-model="ethDialog"
-        :crypto="cryptos()[1].coin"
-        :cryptoName="cryptos()[1].name"
-        :cryptoAddress="cryptos()[1].address"
-      ></crypto-dialog>
-    </v-card>
-
-    <v-card
-      class="support-subcard"
-      tile
-      outlined
-      @click.stop="setLtcDialogOpen(true)"
-    >
-      <v-img :src="`/assets/socials/${cryptos()[2].coin}_button.png`" :alt="cryptos()[2].name"></v-img>
-      <crypto-dialog
-        v-model="ltcDialog"
-        :crypto="cryptos()[2].coin"
-        :cryptoName="cryptos()[2].name"
-        :cryptoAddress="cryptos()[2].address"
-      ></crypto-dialog>
-    </v-card>
-
-    <!-- Alipay -->
-    <v-card
-      class="support-subcard"
-      tile
-      outlined
-      @click.stop="setAliPayDialogOpen(true)"
+      @click.stop="updateTracker(index, alternateDialogTracker)"
     >
       <alternate-payments-dialog
-        v-model="aliPayDialog"
-        name="AliPay"
+        v-model="alternateDialogTracker[index]"
+        :name="name"
       ></alternate-payments-dialog>
-      <v-img src="/assets/socials/AliPay_button.png" alt="AliPay"></v-img>
-    </v-card>
-
-    <!-- WeChat -->
-    <v-card
-      class="support-subcard"
-      tile
-      outlined
-      @click.stop="setWeChatDialogOpen(true)"
-    >
-      <alternate-payments-dialog
-        v-model="weChatDialog"
-        name="WeChat"
-      ></alternate-payments-dialog>
-      <v-img src="/assets/socials/WeChat_button.png" alt="WeChat"></v-img>
+      <v-img :src="`/assets/socials/${name}_button.png`" :alt="name"></v-img>
     </v-card>
   </v-card>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import Vue from "vue";
 import CryptoDialog from "@/components/common/CryptoDialog.vue";
 import AlternatePaymentsDialog from "@/components/common/AlternatePaymentsDialog.vue";
 
-export default defineComponent({
+export default {
   name: "SupportBox",
-  components: {
-    CryptoDialog,
-    AlternatePaymentsDialog,
-  },
-  props: {},
-  setup(props, context) {
-    const btcDialog = ref(false);
-    const ethDialog = ref(false);
-    const ltcDialog = ref(false);
-    const aliPayDialog = ref(false);
-    const weChatDialog = ref(false);
-
-    function setBtcDialogOpen(val: boolean): void {
-      btcDialog.value = val;
-    }
-    function setEthDialogOpen(val: boolean): void {
-      ethDialog.value = val;
-    }
-    function setLtcDialogOpen(val: boolean): void {
-      ltcDialog.value = val;
-    }
-    function setAliPayDialogOpen(val: boolean): void {
-      aliPayDialog.value = val;
-    }
-    function setWeChatDialogOpen(val: boolean): void {
-      weChatDialog.value = val;
-    }
-
-    function alternates(): string[] {
-      return ["AliPay", "WeChat"];
-    }
-
-    function cryptos(): { coin: string; name: string; address: string }[] {
-      return [
+  data() {
+    return {
+      cryptos: [
         {
           coin: `BTC`,
           name: "Bitcoin",
@@ -159,28 +88,25 @@ export default defineComponent({
           name: "Litecoin",
           address: `ltc1q4aq488zph7327nczu3vl3930xu9jke0jr2svh0`,
         },
-      ];
-    }
-
-    return {
-      btcDialog,
-      ethDialog,
-      ltcDialog,
-      aliPayDialog,
-      weChatDialog,
-      setBtcDialogOpen,
-      setEthDialogOpen,
-      setLtcDialogOpen,
-      setAliPayDialogOpen,
-      setWeChatDialogOpen,
-      alternates,
-      cryptos,
+      ],
+      alternates: ["AliPay", "WeChat"],
+      cryptoDialogTracker: [false, false, false],
+      alternateDialogTracker: [false, false],
     }
   },
-});
+  mounted() {},
+  methods: {
+    updateTracker: function(index: number, dialogTracker: boolean[]): void {
+      for (let i = 0; i < dialogTracker.length; i++) {
+        Vue.set(dialogTracker, i, false);
+      }
+      Vue.set(dialogTracker, index, true);
+    }
+  },
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
 .support-subcard {
   border: none !important;
   padding: 1px;
