eda47aed52835039fbb02e2e591689a955f055c1
Gustav Andersson <gustav87and@gmail.com>
2024-05-09 22:24:16 +0200

    migrate GatewaySelect

--- src/components/common/GatewaySelect.vue
+++ src/components/common/GatewaySelect.vue
@@ -3,7 +3,7 @@
     <template v-slot:activator="{ on }">
       <v-btn tile v-on="on" class="transparent">
         <v-icon style="margin-right: 5px">{{ mdiEarth }}</v-icon>
-        {{ gateway }}
+        {{ $t(gateway.name) }}
       </v-btn>
     </template>
     <v-card>
@@ -12,12 +12,12 @@
           {{ $t("components_common_gatewayselect.selectgateway") }}
         </v-subheader>
         <v-list-item
-          v-for="mode in gateWays"
-          :key="mode.gateway"
-          @click="setGateway(mode.gateway)"
+          v-for="gw in gateways"
+          :key="gw.id"
+          @click="gateway = gw"
         >
           <v-list-item-content>
-            <v-list-item-title>{{ mode.name }}</v-list-item-title>
+            <v-list-item-title>{{ $t(gw.name) }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -26,42 +26,45 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import { Component } from "vue-property-decorator";
-
+import { defineComponent, computed, WritableComputedRef, ComputedRef } from "vue";
 import { Gateways } from "@/store/ranking/types";
-import { LocaleMessage } from "vue-i18n";
 import { useRootStateStore } from "@/store/rootState/store";
 import { mdiEarth } from "@mdi/js";
 
-@Component({})
-export default class GatewaySelect extends Vue {
-  public mdiEarth = mdiEarth;
-  private rootStateStore = useRootStateStore();
+export default defineComponent({
+  name: "GatewaySelect",
+  components: {},
+  setup: (_props, context) => {
+    const rootStateStore = useRootStateStore();
 
-  get gateway(): LocaleMessage {
-    const gateway = this.rootStateStore.gateway;
-    return this.gateWays.filter((g) => g.gateway == gateway)[0].name;
-  }
-
-  get gateWays(): Array<{ name: LocaleMessage; gateway: number }> {
-    return [
+    const gateways: Array<{ id: Gateways; name: string }> = [
       {
-        name: this.$t(`gatewayNames.${Gateways[Gateways.Europe]}`),
-        gateway: Gateways.Europe,
+        id: Gateways.Europe,
+        name: `gatewayNames.${Gateways[Gateways.Europe]}`,
       },
       {
-        name: this.$t(`gatewayNames.${Gateways[Gateways.America]}`),
-        gateway: Gateways.America,
+        id: Gateways.America,
+        name: `gatewayNames.${Gateways[Gateways.America]}`,
       },
     ];
-  }
 
-  public setGateway(gateway: Gateways): void {
-    this.rootStateStore.SET_GATEWAY(gateway);
-    this.$emit("gatewayChanged", gateway);
-  }
-}
+    const gateway: WritableComputedRef<{ id: number; name: string }> = computed({
+      get(): { id: number; name: string } {
+        return gateways.find((g) => g.id === rootStateStore.gateway)!;
+      },
+      set(gw: { id: number; name: string }): void {
+        rootStateStore.SET_GATEWAY(gw.id);
+        context.emit("gatewayChanged", gw.id);
+      },
+    });
+
+    return {
+      mdiEarth,
+      gateway,
+      gateways,
+    };
+  },
+});
 </script>
 
 <style></style>
