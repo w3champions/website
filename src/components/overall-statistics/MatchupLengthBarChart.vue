f8dc31dab8cc55abfeee45c04094c0e0a9177902
Gustav Andersson <gustav87and@gmail.com>
2024-05-16 20:59:22 +0200

    migrate MatchupLengthBarChart

--- src/components/overall-statistics/MatchupLengthBarChart.vue
+++ src/components/overall-statistics/MatchupLengthBarChart.vue
@@ -5,62 +5,62 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop } from "vue-property-decorator";
+import { computed, ComputedRef, defineComponent } from "vue";
 import BarChart from "@/components/overall-statistics/BarChart.vue";
-import Vue from "vue";
 import { useOverallStatsStore } from "@/store/overallStats/store";
 import { formatSecondsToDuration } from "@/helpers/date-functions";
-import { MmrRangeValues } from "@/store/overallStats/types";
+import { Length } from "@/store/overallStats/types";
+import { i18n } from "@/main";
+import { ChartData } from "chart.js";
 
-@Component({
-  components: { BarChart },
-})
-export default class PlayerGameLengthStats extends Vue {
-  private statsStore = useOverallStatsStore();
-  @Prop() mmrRange!: MmrRangeValues;
+export default defineComponent({
+  name: "PlayerGameLengthStats",
+  components: {
+    BarChart,
+  },
+  props: {},
+  setup() {
+    const statsStore = useOverallStatsStore();
 
-  get lengths() {
-    const mmrRange = this.statsStore.matchupMmrRange;
-    return this.statsStore.matchupLength?.lengthsByMmrRange?.[mmrRange] || [];
-  }
+    const lengths: ComputedRef<Length[]> = computed((): Length[] => {
+      const mmrRange = statsStore.matchupMmrRange;
+      return statsStore.matchupLength?.lengthsByMmrRange?.[mmrRange] || [];
+    });
 
-  get intervals() {
-    const intervals = this.lengths.map((e) => formatSecondsToDuration(e.seconds));
-    // games in the last position have 60 min or more, so add + as suffix
-    intervals[intervals.length - 1] = intervals[intervals.length - 1] + "+";
-    return intervals.slice(4); // slicing to ignoring first 2 min of game
-  }
+    const games: ComputedRef<number[]> = computed((): number[] => lengths.value.slice(4).map((e) => e.games)); // slicing to ignoring first 2 min of game);
 
-  get games() {
-    return this.lengths.slice(4).map((e) => e.games); // slicing to ignoring first 2 min of game
-  }
+    const intervals: ComputedRef<string[]> = computed((): string[] => {
+      const intervals = lengths.value.map((e) => formatSecondsToDuration(e.seconds));
+      // games in the last position have 60 min or more, so add + as suffix
+      intervals[intervals.length - 1] = intervals[intervals.length - 1] + "+";
+      return intervals.slice(4); // slicing to ignoring first 2 min of game
+    });
 
-  get matchupLengthChartData() {
+    const matchupLengthChartData: ComputedRef<ChartData> = computed((): ChartData => {
+      return {
+        labels: intervals.value,
+        datasets: [
+          {
+            label: String(i18n.t("components_overall-statistics_gamelengthchart.amountofgames")),
+            data: games.value,
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+          },
+        ],
+      };
+    });
+
     return {
-      labels: this.intervals,
-      datasets: [
-        {
-          label: String(
-            this.$t(
-              "components_overall-statistics_gamelengthchart.amountofgames"
-            )
-          ),
-          data: this.games,
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
-          borderColor: "rgba(54, 162, 235, 1)",
-          borderWidth: 1,
-        },
-      ],
+      matchupLengthChartData,
     };
-  }
-
-}
+  },
+});
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .matchup-length {
   margin-right: 24px;
   margin-bottom: 24px;
 }
-
 </style>
