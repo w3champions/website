55c1fbbc780d3145168233c6f0aa3ec5dfefcd53
Gustav Andersson <gustav87and@gmail.com>
2024-05-22 18:43:06 +0200

    fix eslint deprecations in App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,18 @@
       transition="slide-x-transition"
       v-model="navigationDrawerOpen"
     >
-      <v-list dense>
+      <v-list density="compact">
         <v-list-item>
-          <v-list-item-content>
-            <router-link :to="{ name: 'Home' }">
-              <brand-logo :is-dark-theme="isDarkTheme.get()" />
-            </router-link>
-          </v-list-item-content>
-          <v-list-item-icon>
-            <v-icon class="ml-5" @click="setNavigationDrawerOpen(false)">{{ mdiClose }}</v-icon>
-          </v-list-item-icon>
+          <router-link :to="{ name: 'Home' }">
+            <brand-logo :is-dark-theme="isDarkTheme.get()" />
+          </router-link>
+          <template v-slot:prepend="{ isActive }">
+            <v-icon v-if="isActive" class="ml-5" @click="setNavigationDrawerOpen(false)">{{ mdiClose }}</v-icon>
+          </template>
         </v-list-item>
       </v-list>
       <v-divider />
-      <v-list dense nav>
+      <v-list density="compact" nav>
         <v-list-item
           v-for="item in items"
           :key="item.title"
@@ -27,14 +25,12 @@
           link
           :to="{ name: item.to }"
         >
-          <v-list-item-icon>
+          <template v-slot:prepend>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ $t(`views_app.${item.title}`) }}
-            </v-list-item-title>
-          </v-list-item-content>
+          </template>
+          <v-list-item-title>
+            {{ $t(`views_app.${item.title}`) }}
+          </v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -62,7 +58,7 @@
           v-for="item in items"
           v-show="isNavItemVisible(item)"
           :key="item.title"
-          text
+          variant="text"
           tile
           :to="{ name: item.to }"
           :class="item.class"
@@ -77,7 +73,7 @@
 
       <global-search />
 
-      <v-btn text tile @click="loginOrGoToProfile" v-if="!authCode">
+      <v-btn variant="text" tile @click="loginOrGoToProfile" v-if="!authCode">
         <v-icon v-if="!authCode" class="mr-2">{{ mdiAccountCircleOutline }}</v-icon>
         <sign-in-dialog
           v-model="showSignInDialog"
@@ -86,8 +82,8 @@
       </v-btn>
 
       <v-menu v-if="authCode">
-        <template v-slot:activator="{ on }">
-          <v-btn text tile v-on="on">
+        <template v-slot:activator="{ props }">
+          <v-btn variant="text" tile v-bind="props">
             <span class="d-none d-sm-flex mr-2">{{ loginName }}</span>
             <v-icon>{{ mdiAccountCircle }}</v-icon>
           </v-btn>
@@ -102,9 +98,9 @@
         </v-list>
       </v-menu>
 
-      <v-menu offset-y>
-        <template v-slot:activator="{ on }">
-          <v-btn text tile v-on="on" class="right-menu">
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn variant="text" tile v-bind="props" class="right-menu">
             <v-icon>{{ mdiInvertColors }}</v-icon>
           </v-btn>
         </template>
@@ -125,8 +121,8 @@
       </v-menu>
 
       <v-menu>
-        <template v-slot:activator="{ on }">
-          <v-btn text tile v-on="on" style="margin-top: 2px">
+        <template v-slot:activator="{ props }">
+          <v-btn variant="text" tile v-bind="props" style="margin-top: 2px">
             <locale-icon
               :locale="savedLocale.get()"
               :showTwoLetterCode="false"
@@ -148,9 +144,9 @@
     <v-main>
       <router-view />
     </v-main>
-    <v-footer padless class>
+    <v-footer class>
       <v-row justify="center" no-gutters>
-        <v-btn text tile class="my-2" to="/imprint">Imprint</v-btn>
+        <v-btn variant="text" tile class="my-2" to="/imprint">Imprint</v-btn>
       </v-row>
     </v-footer>
   </v-app>
