c84b8b6671f7fbf8cc2a05afd69041ba9cd44758
Gustav Andersson <gustav87and@gmail.com>
2024-05-25 14:22:09 +0200

    add search relevance to global search

--- src/services/GlobalSearchService.ts
+++ src/services/GlobalSearchService.ts
@@ -1,14 +1,14 @@
 import { API_URL } from "@/main";
-import { PlayerSearchData } from "@/store/globalSearch/types";
+import { PlayerSearchInfo } from "@/store/globalSearch/types";
 
 export default class GlobalSearchService {
   // search: the string to search by
-  // lastPlayerId: the last id in the currently loaded list (used for pagination)
+  // lastRelevanceId: the last id in the currently loaded list (used for pagination)
   // pageSize: the number of players to fetch at a time
-  public static async search(search: string, lastPlayerId: string, pageSize: number): Promise<PlayerSearchData[]> {
+  public static async search(search: string, lastRelevanceId: string, pageSize: number): Promise<PlayerSearchInfo[]> {
     let url = `${API_URL}api/players/global-search?search=${encodeURIComponent(search)}&pageSize=${pageSize}`;
-    if (lastPlayerId) {
-      url += `&lastObjectId=${encodeURIComponent(lastPlayerId)}`;
+    if (lastRelevanceId) {
+      url += `&lastRelevanceId=${encodeURIComponent(lastRelevanceId)}`;
     }
 
     const players = await fetch(url).then((response) => response.json());
--- src/store/globalSearch/types.ts
+++ src/store/globalSearch/types.ts
@@ -1,16 +1,17 @@
 import { ProfilePicture } from "@/store/personalSettings/types";
 
 export type GlobalSearchState = {
-  players: PlayerSearchData[];
+  players: PlayerSearchInfo[];
   // False if we fetched all pages of the search
   hasMore: boolean;
 };
 
-export type PlayerSearchData = {
+export type PlayerSearchInfo = {
   battleTag: string;
   name: string;
   seasons: Season[];
   profilePicture: ProfilePicture;
+  relevanceId: string;
 };
 
 export type Season = {
