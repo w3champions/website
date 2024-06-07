4a31b874d6414c5ba9e94d812db49a5fc7b1552d
Konstantin <39737614+healfy@users.noreply.github.com>
2024-06-07 19:42:11 +0300

    drop token params from path (#805)

--- src/services/admin/AdminService.ts
+++ src/services/admin/AdminService.ts
@@ -19,55 +19,55 @@ import { authorizedFetch } from "@/helpers/general";
 
 export default class AdminService {
   public static async getBannedPlayers(token: string): Promise<BannedPlayersResponse> {
-    const url = `${API_URL}api/admin/bannedPlayers?authorization=${token}`;
+    const url = `${API_URL}api/admin/bannedPlayers`;
     const response = await authorizedFetch("GET", url, token);
     return await response.json();
   }
 
   public static async postBan(bannedPlayer: BannedPlayer, token: string): Promise<string> {
-    const url = `${API_URL}api/admin/bannedPlayers/?authorization=${token}`;
+    const url = `${API_URL}api/admin/bannedPlayers`;
     const response = await authorizedFetch("POST", url, token, JSON.stringify(bannedPlayer));
     return response.ok ? "" : await response.json();
   }
 
   public static async deleteBan(bannedPlayer: BannedPlayer, token: string): Promise<string> {
-    const url = `${API_URL}api/admin/bannedPlayers/?authorization=${token}`;
+    const url = `${API_URL}api/admin/bannedPlayers`;
     const response = await authorizedFetch("DELETE", url, token, JSON.stringify(bannedPlayer));
     return response.ok ? "" : (await response.json()).error;
   }
 
   public static async getQueueData(token: string): Promise<QueueData[]> {
-    const url = `${API_URL}api/admin/queue-data/?authorization=${token}`;
+    const url = `${API_URL}api/admin/queue-data`;
     const response = await authorizedFetch("GET", url, token);
     return await response.json();
   }
 
   public static async getAvailableProxies(token: string): Promise<Proxy[]> {
-    const url = `${API_URL}api/admin/proxies/?authorization=${token}`;
+    const url = `${API_URL}api/admin/proxies`;
     const response = await authorizedFetch("GET", url, token);
     return await response.json();
   }
 
   public static async searchByTag(battleTagFragment: string, token: string): Promise<SearchedPlayer[]> {
-    const url = `${API_URL}api/admin/search/${encodeURIComponent(battleTagFragment)}?authorization=${token}`;
+    const url = `${API_URL}api/admin/search/${encodeURIComponent(battleTagFragment)}`;
     const response = await authorizedFetch("GET", url, token);
     return await response.json();
   }
 
   public static async getProxiesForBattletag(battleTag: string, token: string): Promise<ProxySettings> {
-    const url = `${API_URL}api/admin/proxies-for/${encodeURIComponent(battleTag)}?authorization=${token}`;
+    const url = `${API_URL}api/admin/proxies-for/${encodeURIComponent(battleTag)}`;
 
     const response = await authorizedFetch("GET", url, token);
     return await response.json();
   }
 
   public static async putProxies(proxies: ProxySettings, battleTag: string, token: string): Promise<Response> {
-    const url = `${API_URL}api/admin/update-proxies/${encodeURIComponent(battleTag)}?authorization=${token}`;
+    const url = `${API_URL}api/admin/update-proxies/${encodeURIComponent(battleTag)}`;
     return await authorizedFetch("PUT", url, token, JSON.stringify(proxies));
   }
 
   public static async getAltsForBattletag(btag: string, token: string): Promise<string[]> {
-    const url = `${API_URL}api/admin/alts/${encodeURIComponent(btag)}?authorization=${token}`;
+    const url = `${API_URL}api/admin/alts/${encodeURIComponent(btag)}`;
 
     const response = await authorizedFetch("GET", url, token);
 
@@ -75,7 +75,7 @@ export default class AdminService {
   }
 
   public static async getGlobalMutes(token: string): Promise<GloballyMutedPlayer[]> {
-    const url = `${API_URL}api/admin/globalChatBans?authorization=${token}`;
+    const url = `${API_URL}api/admin/globalChatBans`;
 
     const response = await authorizedFetch("GET", url, token);
 
@@ -83,7 +83,7 @@ export default class AdminService {
   }
 
   public static async deleteGlobalMute(token: string, id: string): Promise<number> {
-    const url = `${API_URL}api/admin/globalChatBans/${id}?authorization=${token}`;
+    const url = `${API_URL}api/admin/globalChatBans/${id}`;
 
     const response = await authorizedFetch("DELETE", url, token);
 
@@ -91,19 +91,19 @@ export default class AdminService {
   }
 
   public static async putGlobalMute(token: string, globalMutedPlayer: GlobalMute): Promise<number> {
-    const url = `${API_URL}api/admin/globalChatBans/?authorization=${token}`;
+    const url = `${API_URL}api/admin/globalChatBans`;
     const response = await authorizedFetch("PUT", url, token, JSON.stringify(globalMutedPlayer));
     return response.status;
   }
 
   public static async getAllSpecialPortraits(token: string): Promise<PortraitDefinition[]> {
-    const url = `${API_URL}api/rewards/portrait-definitions?authorization=${token}`;
+    const url = `${API_URL}api/rewards/portrait-definitions`;
     const response = await authorizedFetch("GET", url, token);
     return await response.json();
   }
 
   public static async putPortraits(token: string, command: ChangePortraitsCommand): Promise<number> {
-    const url = `${API_URL}api/rewards/portraits?authorization=${token}`;
+    const url = `${API_URL}api/rewards/portraits`;
 
     const data = {
       BnetTags: command.battleTags,
@@ -116,7 +116,7 @@ export default class AdminService {
   }
 
   public static async deletePortraits(token: string, command: ChangePortraitsCommand): Promise<number> {
-    const url = `${API_URL}api/rewards/portraits?authorization=${token}`;
+    const url = `${API_URL}api/rewards/portraits`;
 
     const data = {
       BnetTags: command.battleTags,
@@ -143,14 +143,14 @@ export default class AdminService {
   }
 
   public static async postPortraitDefinitions(token: string, definitions: PortraitDefinitionDTO): Promise<number> {
-    const url = `${API_URL}api/rewards/portrait-definitions?authorization=${token}`;
+    const url = `${API_URL}api/rewards/portrait-definitions`;
 
     const response = await authorizedFetch("POST", url, token, JSON.stringify(definitions));
     return response.status;
   }
 
   public static async putPortraitDefinitions(token: string, definitions: PortraitDefinitionDTO): Promise<number> {
-    const url = `${API_URL}api/rewards/portrait-definitions?authorization=${token}`;
+    const url = `${API_URL}api/rewards/portrait-definitions`;
 
     const response = await authorizedFetch("PUT", url, token, JSON.stringify(definitions));
 
@@ -158,21 +158,21 @@ export default class AdminService {
   }
 
   public static async deletePortraitDefinitions(token: string, definitions: PortraitDefinitionDTO): Promise<number> {
-    const url = `${API_URL}api/rewards/portrait-definitions?authorization=${token}`;
+    const url = `${API_URL}api/rewards/portrait-definitions`;
 
     const response = await authorizedFetch("DELETE", url, token, JSON.stringify(definitions));
     return response.status;
   }
 
   public static async getChatLog(token: string, matchId: string): Promise<ReplayChatLog> {
-    const url = `${API_URL}api/replays/${matchId}/chats?authorization=${token}`;
+    const url = `${API_URL}api/replays/${matchId}/chats`;
 
     const response = await authorizedFetch("GET", url, token);
     return response.json();
   }
 
   public static async checkJwtLifetime(token: string): Promise<boolean> {
-    const url = `${API_URL}api/admin/checkJwtLifetime?authorization=${token}`;
+    const url = `${API_URL}api/admin/checkJwtLifetime`;
     const response = await authorizedFetch("GET", url, token);
     return response.ok;
   }
