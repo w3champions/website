f740657ae307a8fee06eb86e6c0ad6fd38b900da
Francis Lavoie <lavofr@gmail.com>
2024-10-27 07:28:11 -0400

    Ignore inactive game modes when choosing favourite mode

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -280,9 +280,16 @@ export default defineComponent({
     }
 
     async function initMmrRpTimeline() {
+      // Make a lookup table for active game modes
+      const activeGameModes = rankingsStore.activeModes.reduce((acc, mode) => {
+        acc[mode.id] = true;
+        return acc;
+      }, {} as Record<number, boolean>);
+
       let maxMode = EGameMode.GM_1ON1;
       let maxModeGames = 0;
       playerStore.gameModeStats.forEach((m) => {
+        if (!activeGameModes[m.gameMode]) return;
         if (m.games > maxModeGames) {
           maxModeGames = m.games;
           maxMode = m.gameMode;
