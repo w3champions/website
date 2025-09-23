87254363ad432a3e606391130ee5115004a3f332
Marco de Abreu <marcoabreu@users.noreply.github.com>
2025-09-23 18:45:57 +0200

    Align top definition (#952)
    Co-authored-by: Marco de Abreu <18629099+marcoabreu@users.noreply.github.com>

--- src/components/player/ModeStatsGrid.vue
+++ src/components/player/ModeStatsGrid.vue
@@ -28,12 +28,7 @@
               {{ item.rank !== 0 ? item.mmr : "-" }}
             </div>
             <div v-if="item.rank !== 0" class="sub-value">
-              <v-tooltip top>
-                <template v-slot:activator="{ on }">
-                  <span v-on="on">{{ $t("common.top") }} {{ getTopPercent(item) }}%</span>
-                </template>
-                <span>{{ $t("components_matches_playermatchinfo.quantileTooltip", { percentage: getTopPercent(item) }) }}</span>
-              </v-tooltip>
+              <span>{{ $t("common.top") }} {{ getTopPercent(item) }}%</span>
             </div>
           </td>
           <td class="number-text text-center cell" style="min-width: 100px">
@@ -116,7 +111,8 @@ export default defineComponent({
         return "";
       }
       const quantilePerc = modeStat.quantile * 100;
-      return quantilePerc.toFixed(1);
+      const topPerc = 100 - quantilePerc;
+      return topPerc.toFixed(1);
     }
 
     const headers: ModeStatsGridHeader[] = [
