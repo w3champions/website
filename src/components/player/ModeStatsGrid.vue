330e0ae16970473216c3d45c6e35be0c06734e73
Marco de Abreu <marcoabreu@users.noreply.github.com>
2025-09-23 17:49:29 +0200

    Invert top on match grid (#951)
    * Invert top on match grid
    
    * Align Top function and add tooltip on match stats grid
    
    ---------
    
    Co-authored-by: Marco de Abreu <18629099+marcoabreu@users.noreply.github.com>

--- src/components/player/ModeStatsGrid.vue
+++ src/components/player/ModeStatsGrid.vue
@@ -28,7 +28,12 @@
               {{ item.rank !== 0 ? item.mmr : "-" }}
             </div>
             <div v-if="item.rank !== 0" class="sub-value">
-              {{ getTopPercent(item) }}
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <span v-on="on">{{ $t("common.top") }} {{ getTopPercent(item) }}%</span>
+                </template>
+                <span>{{ $t("components_matches_playermatchinfo.quantileTooltip", { percentage: getTopPercent(item) }) }}</span>
+              </v-tooltip>
             </div>
           </td>
           <td class="number-text text-center cell" style="min-width: 100px">
@@ -111,13 +116,7 @@ export default defineComponent({
         return "";
       }
       const quantilePerc = modeStat.quantile * 100;
-      const topPerc = 100 - quantilePerc;
-
-      if (topPerc > 90) {
-        return "";
-      }
-
-      return `${t("common.top")} ${Math.max(topPerc, 0.1).toFixed(1)}%`;
+      return quantilePerc.toFixed(1);
     }
 
     const headers: ModeStatsGridHeader[] = [
