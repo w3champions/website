a406406a0fbaa067364fe650b121ee1ac3432e4b
Gustav Andersson <gustav87and@gmail.com>
2025-10-21 11:05:03 +0200

    fix AdminMotd, except the input validation rules

--- src/components/admin/AdminMotd.vue
+++ src/components/admin/AdminMotd.vue
@@ -5,7 +5,7 @@
     </v-card-title>
     <v-container>
       <div v-if="!loaded">
-        <v-alert class="ma-4 pa-4" dense outlined type="error">Could not load the Message of the Day</v-alert>
+        <v-alert class="ma-4 pa-4" density="compact" variant="outlined" type="error">Could not load the Message of the Day</v-alert>
       </div>
       <v-card v-else>
         <div v-if="!loading">
@@ -16,7 +16,7 @@
             <v-divider class="mt-4" />
           </v-card-text>
           <v-card-actions class="ma-3 pa-3">
-            <v-textarea v-model="newMotd" outlined counter label="New Motd:" :rules="rules" />
+            <v-textarea v-model="newMotd" variant="outlined" counter label="New Motd:" :rules="rules" />
           </v-card-actions>
           <v-card-actions class="pa-3 ma-3 justify-end">
             <v-btn color="primary" class="w3-race-bg--text" @click="confirmNewMotd">Set New Motd</v-btn>
