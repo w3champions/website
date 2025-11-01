b277c499440798144b5150e77c31826dd3d76ca8
Gustav Andersson <gustav87and@gmail.com>
2025-11-01 14:19:04 +0100

    imprint link colors

--- src/views/Imprint.vue
+++ src/views/Imprint.vue
@@ -21,28 +21,28 @@
               Telephone: 404 857 4571
               <br />
               E-Mail:
-              <a href="mailto:w3champions.team@gmail.com">w3champions.team@gmail.com</a>
+              <a href="mailto:w3champions.team@gmail.com" class="text-primary">w3champions.team@gmail.com</a>
               <br />
               Internet address:
-              <a href="https://w3champions.com" target="_blank">https://w3champions.com</a>
+              <a href="https://w3champions.com" target="_blank" class="text-primary">https://w3champions.com</a>
             </p>
           </v-card-text>
           <v-card-text>
             <h3>Graphics and Image Sources</h3>
             <p>
-              <a href="https://blizzard.gamespress.com/de/Warcraft-III-Reforged-Launch-Press-Kit">
+              <a href="https://blizzard.gamespress.com/de/Warcraft-III-Reforged-Launch-Press-Kit" target="_blank" class="text-primary">
                 https://blizzard.gamespress.com/de/Warcraft-III-Reforged-Launch-Press-Kit
               </a>
             </p>
             <h3>Country flags:</h3>
             <p>
-              <a href="https://github.com/HatScripts/circle-flags" target="_blank" rel="noopener noreferrer">
+              <a href="https://github.com/HatScripts/circle-flags" target="_blank" rel="noopener noreferrer" class="text-primary">
                 https://github.com/HatScripts/circle-flags
               </a>
             </p>
             <h3>VPN and Proxy detection:</h3>
             <p>
-              <a href="https://getipintel.net/" target="_blank" rel="noopener noreferrer">
+              <a href="https://getipintel.net/" target="_blank" rel="noopener noreferrer" class="text-primary">
                 getipintel.net
               </a>
             </p>
