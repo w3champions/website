368eecca2e8fa6c5feecf2f00e705a66f13e0b76
Gustav Andersson <gustav87and@gmail.com>
2025-11-14 14:20:07 +0100

    Transform DownloadReplayIcon from `setup()` syntax to `script setup` (#987)
    * add deprecation comments to eslint.config.mjs
    
    * transform DownloadReplayIcon from `setup()` syntax to `script setup`

--- src/components/matches/DownloadReplayIcon.vue
+++ src/components/matches/DownloadReplayIcon.vue
@@ -19,47 +19,35 @@
   </v-tooltip>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { API_URL } from "@/main";
 import { mdiDownload } from "@mdi/js";
 import { TranslateResult } from "vue-i18n";
 
-export default defineComponent({
-  name: "DownloadReplayIcon",
-  components: {},
-  props: {
-    gameId: {
-      type: String,
-      required: true,
-    },
-  },
-  setup(props) {
-    const { t } = useI18n();
-    const downloading = ref(false);
-    const tooltip = ref<TranslateResult>(t("components_matches_replayicon.download"));
-
-    async function downloadReplay(): Promise<void> {
-      downloading.value = true;
-
-      try {
-        // Download the replay
-        const url = `${API_URL}api/replays/${props.gameId}`;
-        window.open(url, "_self");
-      } catch (error) {
-        console.error("Error downloading replay:", error);
-      } finally {
-        downloading.value = false;
-      }
-    }
-
-    return {
-      mdiDownload,
-      tooltip,
-      downloadReplay,
-      downloading,
-    };
-  },
+const { gameId } = defineProps({
+  gameId: {
+    type: String,
+    required: true,
+  }
 });
+
+const { t } = useI18n();
+const downloading = ref(false);
+const tooltip = ref<TranslateResult>(t("components_matches_replayicon.download"));
+
+async function downloadReplay(): Promise<void> {
+  downloading.value = true;
+
+  try {
+    // Download the replay
+    const url = `${API_URL}api/replays/${gameId}`;
+    window.open(url, "_self");
+  } catch (error) {
+    console.error("Error downloading replay:", error);
+  } finally {
+    downloading.value = false;
+  }
+}
 </script>
