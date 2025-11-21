704f4880a7ba71e68844ac7c71e6ede378155d0c
Gustav Andersson <gustav87and@gmail.com>
2025-11-21 17:45:04 +0100

    move overflow-x-auto class to avoid scrollbar on profile with 3 league cards (#993)

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -2,7 +2,7 @@
   <v-container class="pa-3 w3-container-width">
     <v-row>
       <v-col cols="12">
-        <v-card class="overflow-x-auto" tile>
+        <v-card tile>
           <v-card-title class="pt-3">
             {{ $t("views_app.matches") }}
           </v-card-title>
