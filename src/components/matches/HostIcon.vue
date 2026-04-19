598b3cd13d5bb514dae74578e840ded8d193cd37
Gustav Andersson <gustav87and@gmail.com>
2026-04-19 12:05:46 +0200

    migrate HostIcon to script setup syntax

--- src/components/matches/HostIcon.vue
+++ src/components/matches/HostIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <v-tooltip location="bottom" content-class="w3-tooltip elevation-1">
     <template v-slot:activator="{ props }">
-      <div class="globe" v-bind="props">
+      <div v-bind="props">
         <v-img :src="icon" :width="18" :height="18" />
       </div>
     </template>
@@ -15,55 +15,38 @@
   </v-tooltip>
 </template>
 
-<script lang="ts">
-import { computed, PropType, defineComponent } from "vue";
+<script setup lang="ts">
+import { computed, PropType } from "vue";
 import { useI18n } from "vue-i18n";
 import { TranslateResult } from "vue-i18n";
 import { getAsset } from "@/helpers/url-functions";
 import { ServerInfo } from "@/store/types";
 
-export default defineComponent({
-  name: "HostIcon",
-  components: {},
-  props: {
-    host: {
-      type: Object as PropType<ServerInfo>,
-      required: true,
-    },
-  },
-  setup(props) {
-    const { t } = useI18n();
-
-    const icon = computed<string>(() => {
-      if (!props.host) return getAsset("icons/hosterror.png");
-      return getAsset(`icons/${props.host.provider}.png`);
-    });
-
-    const tooltip = computed<TranslateResult>(() => {
-      if (!props.host) return t("components_matches_hosticon.error");
-      if (props.host.provider === "BNET") return t("components_matches_hosticon.hostedonbnet");
-      return `${t("components_matches_hosticon.hostedonflo")} / ${props.host.name}`;
-    });
-
-    function stripTag(tag: string): string {
-      if (!tag) return "";
-
-      const hashIndex = tag.indexOf("#");
-      if (hashIndex != -1) return tag.substring(0, hashIndex);
-      return tag;
-    }
-
-    return {
-      icon,
-      tooltip,
-      stripTag,
-    };
-  },
+const { host } = defineProps({
+  host: {
+    type: Object as PropType<ServerInfo>,
+    required: true,
+  }
 });
-</script>
 
-<style lang="scss" scoped>
-.globe {
-  display: inline-block;
+const { t } = useI18n();
+
+const icon = computed<string>(() => {
+  if (!host) return getAsset("icons/hosterror.png");
+  return getAsset(`icons/${host.provider}.png`);
+});
+
+const tooltip = computed<TranslateResult>(() => {
+  if (!host) return t("components_matches_hosticon.error");
+  if (host.provider === "BNET") return t("components_matches_hosticon.hostedonbnet");
+  return `${t("components_matches_hosticon.hostedonflo")} / ${host.name}`;
+});
+
+function stripTag(tag: string): string {
+  if (!tag) return "";
+
+  const hashIndex = tag.indexOf("#");
+  if (hashIndex != -1) return tag.substring(0, hashIndex);
+  return tag;
 }
-</style>
+</script>
