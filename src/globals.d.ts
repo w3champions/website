8ca4f163cb2e91fc89907213f1e978ed90d050b6
Gustav Andersson <gustav87and@gmail.com>
2026-04-28 16:49:22 +0200

    add `globals.d.ts` for module declations

---
+++ src/globals.d.ts
@@ -0,0 +1,5 @@
+// https://github.com/vuetifyjs/vuetify/issues/22766
+// https://www.typescriptlang.org/tsconfig/#noUncheckedSideEffectImports
+
+declare module "vuetify/styles";
+declare module "@/scss/main.scss";
