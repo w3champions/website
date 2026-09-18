c9bbfb97cd9cc39698c05464cca47cbbca62b244
godrien <kledilson.ferreira@gmail.com>
2026-09-18 08:42:48 -0300

    fix: add 1 day cache busting to filter heroes call (#1127)

--- src/services/HeroService.ts
+++ src/services/HeroService.ts
@@ -4,7 +4,8 @@ import { HeroFilter } from "@/store/heroes";
 export default class HeroService {
   public static async retrieveHeroes(): Promise<HeroFilter[]> {
     const url = `${API_URL}api/hero/filters`;
-    const response = await fetch(url);
+    const dateString = new Date().toISOString().split("T")[0];
+    const response = await fetch(`${url}?date=${dateString}`);
     return await response.json();
   }
 }
